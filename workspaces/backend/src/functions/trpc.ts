56e7d880e61677d54b6e230d77a8cbd0e50dac60
Sam Huynh <samhwang2112.dev@gmail.com>
2023-03-07 02:33:59 +1100

    feat: use the trpc-netlify-functions package

--- workspaces/backend/src/functions/trpc.ts
+++ workspaces/backend/src/functions/trpc.ts
@@ -1,6 +1,6 @@
+import { netlifyTRPCHandler } from 'trpc-netlify-functions';
 import { createContext } from '../trpc/context';
 import { appRouter } from '../trpc/router';
-import { netlifyTRPCHandler } from '../trpc/netlify-functions-adapter';
 
 export const handler = netlifyTRPCHandler({
   router: appRouter,
